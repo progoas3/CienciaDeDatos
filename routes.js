f25d7918f1f95ec934e8a2011aa9a06a7dee36a7
Julian Prieto <ludicolo2209@hotmail.com>
2022-10-18 21:44:42 -0500

    first commit

---
+++ routes.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const routes = express.Router()
+
+//rutas
+
+routes.get('/:numero_accion', (req, res)=>{
+    req.getConnection((err, conn)=> {
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM seguimiento where numero_accion = ?',[req.params.numero_accion], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+} )
+
+
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=> {
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO seguimiento set ?',[req.body], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('seguimiento insert')
+        })
+    })
+} )
+
+routes.delete('/:numero_accion', (req, res)=>{
+    req.getConnection((err, conn)=> {
+        if(err) return res.send(err)
+
+        conn.query('DELETE FROM seguimiento WHERE numero_accion = ?', [req.params.numero_accion], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Eliminado')
+        })
+    })
+} )
+
+routes.put('/:numero_accion', (req, res)=>{
+    req.getConnection((err, conn)=> {
+        if(err) return res.send(err)
+
+        conn.query('UPDATE seguimiento set ? WHERE numero_accion = ?', [req.body, req.params.numero_accion], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Modificado')
+        })
+    })
+} )
+module.exports = routes
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mysql  = require('mysql')
+const myconn = require('express-myconnection')
+
+const routes = require('./routes')
+
+const app = express()
+app.set('port', process.env.PORT || 9000)
+
+
+const dbOptions = {
+    host: 'sql10.freemysqlhosting.net',
+    port: 3306,
+    user: 'sql10527072',
+    password: '8e1QkGTruM',
+    database: 'sql10527072'
+}
+
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+//rutas
+app.get('/', (req, res)=>{
+    res.send('Welcome')
+})
+
+app.use('/seguimiento', routes)
+
+
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port', app.get('port'))
+})
