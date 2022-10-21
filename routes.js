3a750d561a90c57f3c6cf908876258029d6d5633
Julian Prieto <ludicolo2209@hotmail.com>
2022-10-20 19:11:49 -0500

    xd

--- routes.js
+++ routes.js
@@ -38,23 +38,14 @@ routes.get('/', cors(corsOptions), (req, res)=>{
 
 
 
-routes.post('/', cors(corsOptions),(req, res)=>{
+routes.post('/',cors(corsOptions),(req, res)=>{
     req.getConnection((err, conn)=> {
         if(err) return res.send(err)
 
         conn.query('INSERT INTO seguimiento set ?',[req.body], (err, rows)=>{
             if(err) return res.send(err)
-            if(err || !articulo)
-            {
-                return res.status(500).json({
-                    status: "Error",
-                    mensaje: "Metodo no borrado"
-                })
-            } 
-            return res.status(200).json({
-                status: "Success",
-                mensaje: "Metodo  borrado"
-            })
+
+            res.send('seguimiento insert')
         })
     })
 } )
